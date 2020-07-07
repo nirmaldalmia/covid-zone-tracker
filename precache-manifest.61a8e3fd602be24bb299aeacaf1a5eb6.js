5d76d6cca82ddc29323edf9f3c5944fb0f8e8679
nirmaldalmia <nirmaldalmia17@gmail.com>
2020-07-07 16:20:04 +0530

    Updates

---
+++ precache-manifest.61a8e3fd602be24bb299aeacaf1a5eb6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f2d3d943bda946e99c8912c639bf6e",
+    "url": "/covid-zone-tracker/index.html"
+  },
+  {
+    "revision": "c5df79e1989ee0c32179",
+    "url": "/covid-zone-tracker/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "40f6470fe1d7aa856730",
+    "url": "/covid-zone-tracker/static/css/main.29547c45.chunk.css"
+  },
+  {
+    "revision": "c5df79e1989ee0c32179",
+    "url": "/covid-zone-tracker/static/js/2.1635958c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-zone-tracker/static/js/2.1635958c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40f6470fe1d7aa856730",
+    "url": "/covid-zone-tracker/static/js/main.6a28be5c.chunk.js"
+  },
+  {
+    "revision": "1e9db469d76802b0570e",
+    "url": "/covid-zone-tracker/static/js/runtime-main.f280528d.js"
+  },
+  {
+    "revision": "e45f1243a68b9409ba8d27fa69177763",
+    "url": "/covid-zone-tracker/static/media/sanitization-bg.e45f1243.png"
+  }
+]);
